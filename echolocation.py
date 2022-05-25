417cc7c8c0eaa0349a60e23283a44a66aca533d9
Karol <42039707+KarolKob@users.noreply.github.com>
2022-05-25 13:03:29 +0200

    Circle points calculation

--- echolocation.py
+++ echolocation.py
@@ -1,5 +1,6 @@
 from sympy import *
 from sympy.geometry import *
+
 x = Point(0, 0)
 y = Point(1, 1)
 z = Point(2, 2)
@@ -24,8 +25,47 @@ pb1, pb2, pb3, pb4 = [(0, 4), (-4, 0), (0, -4), (4, 0)]
 sqb = Polygon(pb1, pb2, pb3, pb4)
 print(sqb.area)
 
-x = Point(0, 0)
-cir = Circle(x, 2)
-cir.arbitrary_point()
+
+radius = 2
+x = -2
+cir_point_array = []
+sampling_param = 0.4    # 0.4 for 20 samples overall
+
+print("Circle time")
+count = 0
+# Circle function y^2 = r^2 - x^2
+# Count the first half of the sensor trajectory
+while x < radius:
+    y_square = pow(radius, 2) - pow(x, 2)
+
+    if y_square == 0:
+        y = 0
+    else:
+        y = sqrt(y_square)
+
+    cir_point = Point(x, y)
+    cir_point_array.append(cir_point)
+    x = x + 0.4
+    count = count + 1
+
+print(count)
+
+x = radius
+
+while x > -radius:
+    y_square = pow(radius, 2) - pow(x, 2)
+
+    if y_square == 0:
+        y = 0
+    else:
+        y = -sqrt(y_square)
+
+    cir_point = Point(x, y)
+    cir_point_array.append(cir_point)
+    x = x - 0.4
+    count = count + 1
+
+print(count)
+print(cir_point_array)
 
 # Virtual sources method for simulation of indoor acoustics
